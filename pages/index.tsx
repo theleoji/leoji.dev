218307a9d3163b1e20b97a6e3be9bdc936593b2a
Leo Ji <leo@leoji.dev>
2019-09-23 21:47:08 -0400

    introduce a section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Layout from '../components/layout'
+import Section from '../components/section'
 
 const Home = () => (
   <Layout>
@@ -8,14 +9,16 @@ const Home = () => (
       <title>Leo Ji, software engineer and news nerd</title>
       <meta property="og:url" content="{serverRuntimeConfig.domain + '/'}" />
     </Head>
-    <h1 className="title">Leo Ji</h1>
-    <h2 className="subtitle">software engineer, news nerd</h2>
-    <p>I'm currently a software engineer at The Washington Post. </p>
-    <p>
-      I graduated in March 2019 from Northwestern University with a major in
-      journalism, a minor in computer science, and a certificate in
-      human-centered design.
-    </p>
+    <Section>
+      <h1 className="title">Leo Ji</h1>
+      <h2 className="subtitle">software engineer, news nerd</h2>
+      <p>I'm currently a software engineer at The Washington Post. </p>
+      <p>
+        I graduated in March 2019 from Northwestern University with a major in
+        journalism, a minor in computer science, and a certificate in
+        human-centered design.
+      </p>
+    </Section>
   </Layout>
 )
 
