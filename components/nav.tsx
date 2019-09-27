c40de2e2174969a7dfee2ab42bb14861248fde2a
Leo Ji <leo@leoji.dev>
2019-09-27 16:29:06 -0400

    Revert "introduce a section"
    This reverts commit 218307a9d3163b1e20b97a6e3be9bdc936593b2a.

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,43 +1,38 @@
 import React from 'react'
-
-import { Drawer } from '@material-ui/core'
-
 import Link from 'next/link'
 
 export default class Nav extends React.Component {
   render() {
     return (
-      <Drawer>
-        <nav>
-          <ul className="nav">
-            <li>
-              <Link href="/">
-                <a>
-                  home <br />
-                  <small>where the ~ is</small>
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="https://leoji.blog/">
-                <a target="_blank" rel="noopener">
-                  blog <br />
-                  <small>where i say things</small>
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-                <a>
-                  contact me
-                  <br />
-                  <small>to say nice things</small>
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </Drawer>
+      <nav>
+        <ul className="nav">
+          <li>
+            <Link href="/">
+              <a>
+                home <br />
+                <small>where the ~ is</small>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://leoji.blog/">
+              <a target="_blank" rel="noopener">
+                blog <br />
+                <small>where i say things</small>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>
+                contact me
+                <br />
+                <small>to say nice things</small>
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     )
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
 import Layout from '../components/layout'
-import Section from '../components/section'
 
 const Home = () => (
   <Layout>
@@ -9,16 +8,14 @@ const Home = () => (
       <title>Leo Ji, software engineer and news nerd</title>
       <meta property="og:url" content="{serverRuntimeConfig.domain + '/'}" />
     </Head>
-    <Section>
-      <h1 className="title">Leo Ji</h1>
-      <h2 className="subtitle">software engineer, news nerd</h2>
-      <p>I'm currently a software engineer at The Washington Post. </p>
-      <p>
-        I graduated in March 2019 from Northwestern University with a major in
-        journalism, a minor in computer science, and a certificate in
-        human-centered design.
-      </p>
-    </Section>
+    <h1 className="title">Leo Ji</h1>
+    <h2 className="subtitle">software engineer, news nerd</h2>
+    <p>I'm currently a software engineer at The Washington Post. </p>
+    <p>
+      I graduated in March 2019 from Northwestern University with a major in
+      journalism, a minor in computer science, and a certificate in
+      human-centered design.
+    </p>
   </Layout>
 )
 
